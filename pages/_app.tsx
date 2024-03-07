19133dd2327e5ed0bf5a4121a1f00d3c655688aa
swayz8148 <michealdownard07@gmail.com>
2024-03-07 11:34:30 +0000

    Add @vercel/speed-insights package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { extendTheme } from "@chakra-ui/react";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const theme = extendTheme({
   fonts: {
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </ChakraProvider>
   );
 }
