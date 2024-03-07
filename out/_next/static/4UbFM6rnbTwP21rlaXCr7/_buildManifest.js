b2544131311a82f87bc258b9ddf3591e28e6fe63
swayz8148 <michealdownard07@gmail.com>
2024-03-07 08:18:11 +0000

    Update .gitignore file

---
+++ out/_next/static/4UbFM6rnbTwP21rlaXCr7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/4577d2ec-f3ca85288bfff198.js","static/chunks/pages/index-4a452d25f0338f44.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
