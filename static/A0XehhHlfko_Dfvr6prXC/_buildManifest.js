9fa5d1445af9a40ed62e2540065e07537d2b45e5
swayz8148 <michealdownard07@gmail.com>
2024-03-07 04:31:57 +0000

    CV

---
+++ static/A0XehhHlfko_Dfvr6prXC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/4577d2ec-f3ca85288bfff198.js","static/chunks/421-fcecbc89b1a517dc.js","static/chunks/pages/index-2c66512e4a101b54.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
