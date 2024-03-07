3079ade329c1d379d28ac037030b84ace6ad5d95
swayz8148 <michealdownard07@gmail.com>
2024-03-07 07:38:53 +0000

    Add base path and asset prefix for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/https://swayz8148.github.io/',
+  assetPrefix: '/https://swayz8148.github.io/',
   output: 'export',
   reactStrictMode: true,
   images: {
